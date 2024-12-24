d38f6327e0b56632a0b3c3b58e8ca0dbb3bb90f8
jhcpu4 <yxueb@jhcpu4>
2024-12-24 15:43:45 +0800

    update training bash

--- scripts/train_motcat.sh
+++ scripts/train_motcat.sh
@@ -1,5 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --data_root_dir /path/to/feature \
+--use_micro_batch \
+--bs_micro 4096 \
 --split_dir tcga_brca \
 --model_type motcat \
 --ot_impl pot-uot-l2 \
